4db8b7fa37d3f03523bc63c2832f5413d5d882f9
sam <samgathon@hotmail.com>
2019-09-10 09:36:48 -0500

    add car models

---
+++ main_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Car
+
+# View functions
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def cars_index(request):
+  cars = Car.objects.all()
+  return render(request, 'cars/index.html', { 'cars': cars })
+
+def cars_detail(request, car_id):
+  car = Car.objects.get(id=car_id)
+  return render(request, 'cars/detail.html', { 'car': car })
